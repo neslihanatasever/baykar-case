964e734e67aff6c6e3d079a46dae6e7ce55144d0
neslihanatasever <neslihanatasvr111@gmail.com>
2024-03-04 18:51:06 +0300

    [#2]: header yapıldı

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'browncol': '#78350F'
+        }
+      },
     },
     plugins: [],
   }
