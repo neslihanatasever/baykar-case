27a8d0a80a0e6b32ebbf6a0a280a5a16306e2a99
neslihanatasever <neslihanatasvr111@gmail.com>
2024-03-08 00:21:09 +0300

    [#10] boşluklar temizlendi

--- css-case/index.js
+++ css-case/index.js
@@ -1,34 +1,33 @@
 const swiper = new Swiper(".swiper-command", {
-    slidesPerView: 1.2,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1.2,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 50,
-      },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-  });
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
 
-  document.getElementById('menu-toggle').addEventListener('click', function() {
-    document.getElementById('mobile-menu').classList.toggle('hidden');
-  });
-  
- 
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.getElementById('mobile-menu').classList.toggle('hidden');
+});
+
