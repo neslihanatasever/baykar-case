56fbb6274d90d655b718eb1579f79eb05f3518ea
neslihanatasever <neslihanatasvr111@gmail.com>
2024-03-07 22:49:12 +0300

    [#10] slider olan kısım responsive yapıldı

--- css-case/index.js
+++ css-case/index.js
@@ -1,5 +1,5 @@
 const swiper = new Swiper(".swiper-command", {
-    slidesPerView: 4,
+    slidesPerView: 1.2,
     spaceBetween: 30,
     loop: true,
     navigation: {
@@ -11,9 +11,24 @@ const swiper = new Swiper(".swiper-command", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
   });
 
   document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('mobile-menu').classList.toggle('hidden');
   });
   
+ 
